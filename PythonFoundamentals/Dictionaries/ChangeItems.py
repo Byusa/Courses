d73c40d9d3e980c0dbfa30c2607559a460e25de6
Beza Betty <bezabetty@Bezas-Air.home>
2022-12-08 11:59:23 -0800

    added the changeItems, accessitems for dict

---
+++ PythonFoundamentals/Dictionaries/ChangeItems.py
@@ -0,0 +1,15 @@
+# Change values
+# you can change the value of any item by referencing the key name 
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = "1999"
+print(thisdict)
+
+# update dictionary
+# update()
+thisdict.update({"year": 2022})
+print(thisdict)
+
