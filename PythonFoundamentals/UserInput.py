cddcd1fb388072947f4c0ffcff319b50c245b1f1
byusa <byusa.serge@gmail.com>
2022-12-15 14:57:59 -0800

    Added Pip and user input

--- PythonFoundamentals/UserInput.py
+++ PythonFoundamentals/UserInput.py
@@ -1,4 +1,10 @@
 # UserInput
+# python 2.7
+# username = raw_input("Enter your name ")
+# print("your name is: " + username)
 # python 3.6
 username = input("Enter your name ")
 print("your name is: " + username)
+
+
+
