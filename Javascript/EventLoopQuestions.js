e20de6d2ebe141336ed960bfd90f8b9193a16352
Serge Byusa <sergebyusa@Serges-MBP.home>
2023-01-09 20:52:01 -0800

    Added the EventLoop question setInterval and setTimeout

--- Javascript/EventLoopQuestions.js
+++ Javascript/EventLoopQuestions.js
@@ -1,14 +1,16 @@
 let a = true
+let num = 0
+
+let id = setInterval(() => {
+    console.log(num++) // prints up to 8
+}, 200) // print a every two second
+
 setTimeout(()=> {
-    a = false;
+    clearInterval(id) // will let the cursor free after 2 sec
 }, 2000) // after 2 sec a = false
 
-// setInterval(() => {
-//     if(a) {
-//         console.log('Hello')
-//     }
-// }, 200) // print a every two second
 
-while(a){
-    console.log("sup")
-}
+
+// while(a){
+//     console.log(num++) // the thread is never free to let setTimeout give results
+// }
